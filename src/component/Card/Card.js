1ef2b74ad44fc9619535251eff0d855ef13b696e
FrankenDeba <debashishrambhola@gmail.com>
2021-05-02 01:10:30 +0530

    test

---
+++ src/component/Card/Card.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./CardStyle.css"
+
+function Card(props) {
+    const { data } = props
+    const status = data.hasStarted ? 
+                            <div className = "status">
+                                <p className = "no__of__done">{data.noOfDone}</p> 
+                                <p>&nbsp;/&nbsp;{data.noOfQuestions} </p> 
+                                <p className = "no__of__questions">has done</p>
+                            </div>
+                            :
+                            <p className = "not__started__text">not yet started</p>
+    return (
+        <div className = "card__container">
+            <p className = "topic">{data.category}</p>
+            <div className = "questions__count">
+                <p className = "total__question__para">Total Questions: </p>
+                <p className = "no__of__questions">{data.noOfQuestions}</p>
+            </div>
+            {status}
+        </div>
+    )
+}
+
+export default Card
